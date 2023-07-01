dbb1d52033b4793e853ccece9c7a698af34d9ef3
Erik Kallen <info@erikkallen.nl>
2023-07-01 12:50:26 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-5xl text-center pt-10 text-slate-600">Welcome to <span class="font-semibold text-orange-600">EVA</span>CRAFT</h1>
+
+    
+
+    <div class="flex flex-col items-center justify-center">
+      <a class="my-10 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" href="https://github.com/EVAGAMEN/evacraft/raw/main/EVACRAFT%201.20.1-1.0.zip">
+      <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+
+        <span>      Download GDlauncher instance</span></a>
+      <img src="/map_1335853976.png" alt="World map" class="w-1/2" />
+    </div>
+  </div>
+</template>
